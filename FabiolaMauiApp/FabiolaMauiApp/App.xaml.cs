21258e48b4ce8514dec7bd70a4438c0e897d1897
Fabíola Martins <fabiola.andrade.martins@outlook.com>
2024-10-13 08:40:47 +0100

    Added Controls that define values

--- FabiolaMauiApp/FabiolaMauiApp/App.xaml.cs
+++ FabiolaMauiApp/FabiolaMauiApp/App.xaml.cs
@@ -13,7 +13,7 @@ namespace FabiolaMauiApp
             //navPage.BarBackground = Colors.Yellow;
             //navPage.BarTextColor = Colors.Blue;
 
-            MainPage = new NavigationPage(new MauiCommandControls());
+            MainPage = new NavigationPage(new MauiSetValueControls());
         }
     }
 }
